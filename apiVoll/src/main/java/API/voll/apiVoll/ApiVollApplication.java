3bd0a66cb6eea11490b2859bd1d0c189e75bc525
Evandro Mattos <emattos0114@outlook.com>
2024-07-11 11:18:29 -0300

    first commit

---
+++ apiVoll/src/main/java/API/voll/apiVoll/ApiVollApplication.java
@@ -0,0 +1,13 @@
+package API.voll.apiVoll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiVollApplication.class, args);
+	}
+
+}
---
+++ apiVoll/src/main/java/API/voll/apiVoll/controller/HelloController.java
@@ -0,0 +1,15 @@
+package API.voll.apiVoll.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping
+    public String olaMundo(){
+        return "Hello World! ";
+    }
+}
