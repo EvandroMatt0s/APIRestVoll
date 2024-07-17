d704f591474875d611b6cc291fd91f4f3e7cfa76
Evandro Mattos <emattos0114@outlook.com>
2024-07-17 18:18:57 -0300

    Paginação

--- apiVoll/src/main/java/API/voll/apiVoll/controller/MedicoController.java
+++ apiVoll/src/main/java/API/voll/apiVoll/controller/MedicoController.java
@@ -6,6 +6,8 @@ import API.voll.apiVoll.medico.Medico;
 import API.voll.apiVoll.medico.MedicoRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,7 +26,7 @@ public class MedicoController {
         repository.save(new Medico(dados));
     }
     @GetMapping
-    public List<DadosListagemMedico>listar(){
-        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+    public Page<DadosListagemMedico> listar(Pageable paginacao){
+        return repository.findAll(paginacao).map(DadosListagemMedico::new);
     }
 }
